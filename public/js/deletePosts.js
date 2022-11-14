a69191d7cb558ac5f5ccb1e145975de523745c47
MarioArmstrong <arms.mario@icloud.com>
2022-11-13 22:14:06 -0800

    messing with delteBtn. still not working

--- public/js/deletePosts.js
+++ public/js/deletePosts.js
@@ -1,23 +1,19 @@
-// THIS ISN'T FINISHED. COPIED FROM CREATEPOST TO HAVE SOMETHING HERE
-
 const deletePostHandler = async (event) => {
   event.preventDefault();
+  console.log("you clicked it!");
+  const btn = document.querySelectorAll(".deleteBtn");
+  const id = btn.getAttribute("data-id");
 
-  const id = event.target.getAttribute("data-id");
-  if (event.target.hasAttribute("data-id")) {
-    const response = await fetch(`/api/posts/${id}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-    });
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
 
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert("Failed to delete post");
-    }
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert("Failed to delete post");
   }
 };
 
-document
-  .getElementsByClassName("deleteBtn")
-  .addEventListener("click", deletePostHandler);
+btn.addEventListener("click", deletePostHandler);
